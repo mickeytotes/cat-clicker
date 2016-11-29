0bc4a7e29a7663eafcccba86027a3f44d8bbb84e
Michael Satow <satow.m@gmail.com>
2016-11-29 11:33:18 -0500

    remove commented out functions

--- js/clicker.js
+++ js/clicker.js
@@ -38,30 +38,6 @@ function loadCats(array) {
 		catImg.id = array[i].imgid;
 		catCount.id = array[i].countid;
 
-		/*catImg.addEventListener("click", function() {
-			catCount.innerHTML = Number(catCount.innerHTML) + 1;
-			console.log(catCount.innerHTML);
-			console.log(event.target);
-			console.log(this);
-		}, false);*/
-
-/*	catImg.addEventListener("click",function(){
-		var index = i;
-		var elem = document.getElementsByClassName('cat');
-	    array[index].count = array[index].count + 1;
-	    var total= array[index].count;
-	    var numb = document.getElementsByTagName("p")[index];
-	    numb.innerHTML = total;
-	},false);*/
-
-//for (var i; i < array.length; i++){
-//		var image = array[i].image;
-//		image.addEventListener("click", function() {
-//			var count = array[i].count;
-//			count.innerHTML = Number(count.innerHTML) + 1;
-//			console.log(count.innerHTML);
-//		}, false);
-
 		document.body.appendChild(catEntry);
 	}
 
@@ -80,17 +56,6 @@ function loadCats(array) {
 			console.log(catCount2.innerHTML);
 		}, false);
 }
-/*var catImage1 = cats.cat[0];
-
-catImage1.addEventListener("click", fuction() {
-	var clickCount = catImage1.count;
-	clickCount = Number(clickCount) + 1;
-}, false);*/
 
 loadCats(cats);
-
-
-//var num = document.getElementById('counter');
-//		num.innerHTML = Number(num.innerHTML) + 1;
-//		console.log(num.innerHTML);
 
