8b826c3b79c35634ba5f70ffd9ccde2085e7dea8
Michael Satow <satow.m@gmail.com>
2016-12-05 11:16:24 -0500

    remove unnecessary or commented out code

--- js/clicker.js
+++ js/clicker.js
@@ -6,37 +6,37 @@
 var cats = [{
 		name: "Toby",
 		img: "images/happy-kitten.jpg",
-		count: 0,
-		imgid: 1,
-		countid: 3
+		count: 0
+		//imgid: 1,
+		//countid: 3
 	},
 	{
 		name: "Cecile",
 		img: "images/screaming.jpeg",
-		count: 0,
-		imgid: 2,
-		countid: 4
+		count: 0
+		//imgid: 2,
+		//countid: 4
 	},
 	{
 		name: "Johnson",
 		img: "images/bald-cat.jpg",
-		count: 0,
-		imgid: 5,
-		countid: 6
+		count: 0
+		//imgid: 5,
+		//countid: 6
 	},
 	{
 		name: "Olivia",
 		img: "images/waiting-cat.jpg",
-		count: 0,
-		imgid: 7,
-		countid: 8
+		count: 0
+		//imgid: 7,
+		//countid: 8
 	},
 	{
 		name: "Stormio",
 		img: "images/duh-cat.jpg",
-		count: 0,
-		imgid: 9,
-		countid: 10
+		count: 0
+		//imgid: 9,
+		//countid: 10
 	}]
 
 function loadCats(array) {
@@ -58,7 +58,6 @@ function loadCats(array) {
 				var counter = document.getElementById('sample-count');
 				var p = document.getElementById('sample-p');
 				var cat = document.getElementById("now-cat");
-				//counter.innerHTML = countCopy;
 				p.innerHTML = '';
 				counter.innerHTML = '';
 				cat.innerHTML = '';
@@ -103,52 +102,3 @@ function loadCats(array) {
 }
 
 loadCats(cats);
-
-/*function loadNames(array) {
-	for (var i = 0; i < array.length; i++) {
-		var name = document.createElement('li');
-		name.innerHTML = array[i].name;
-
-		name.addEventListener("click", (function(nameCopy) {
-			return function() {
-				var nameValue = nameCopy.innerHTML;
-				document.getElementById('sample-box').innerHTML = '';
-				document.getElementById('sample-box').append(nameValue);
-				console.log(nameCopy);
-			};
-		})(name));
-
-		document.getElementById('name-container').appendChild(name);
-	}
-}*/
-
-//---------------------------------------
-//scratch
-/*function loadNames(array) {
-	for (var i = 0; i < array.length; i++) {
-		var name = document.createElement('li');
-		name.innerHTML = array[i].name;
-		var img = array[]
-
-		name.addEventListener("click", (function(nameCopy) {
-			return function() {
-				var nameValue = nameCopy.innerHTML;
-				document.getElementById('sample-box').innerHTML = '';
-				document.getElementById('sample-box').append(nameValue);
-				console.log(nameCopy);
-			};
-		})(name));
-
-		document.getElementById('name-container').appendChild(name);
-	}
-}*/
-
-/*
-
-function getSelectedCat(array) {
-
-}
-
-
-*/
-
